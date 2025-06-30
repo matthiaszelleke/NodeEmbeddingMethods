83f2abb2b0ed97664d0a5eaa079048083d47ad01
Matthias Zelleke <mza151@sfu.ca>
2025-06-30 12:03:26 -0700

    adding master file and network simulator

---
+++ run_all.py
@@ -0,0 +1,70 @@
+import subprocess
+import csv
+
+FILENAME = "nmi_scores.csv"
+
+with open(FILENAME, "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Method", "NMI Score"])
+
+    # Creating the Stochastic Block Model network
+    # The network will be stored in an edgelist file
+    subprocess.run(["python", "sbm_simulator.py"], check=True)
+
+
+    # Learning the SBM's node embeddings using spectral clustering and running kmeans
+    # on them and extracting the NMI score
+    print("Running Spectral Clustering...")
+    result_spectral = subprocess.run(["python", "SpectralClusteringImplementation/laplacian_eigenmap.py"], 
+                                     capture_output=True, text=True, check=True)
+    output_spectral = result_spectral.stdout.strip()
+    nmi_spectral = output_spectral.split("NMI Score:")[-1].strip() # obtaining last chunk of text (hopefully the float representing the NMI)
+
+    writer.writerow(["Spectral Clustering", nmi_spectral])
+
+
+    # Learning the node embeddings using the node2vec method
+    print("Running node2vec...")
+    subprocess.run(["python", "node2vecImplementation/n2v_embedding_gen.py"], check=True)
+
+    # Running kmeans on the embeddings and extracting the NMI score
+    result_n2v = subprocess.run(["python", "node2vecImplementation/n2v_kmeans_eval.py"], 
+                                capture_output=True, text=True, check=True)
+    output_n2v = result_n2v.stdout.strip()
+    nmi_n2v = output_n2v.split("NMI Score:")[-1].strip() # obtaining last chunk of text (hopefully the float representing the NMI)
+
+    writer.writerow(["Node2vec", nmi_n2v])
+
+
+    # Learning the node embeddings using LINE method
+    print("Running LINE...") 
+    line_subprocess = subprocess.Popen(["python", "LINEImplementation/MyImplementation/train.py", "-g", 
+                      "./sbm_graph.edgelist", "-save", "LINEImplementation/MyImplementation/LINE_model.pt", 
+                      "-lossdata", "LINEImplementation/MyImplementation/loss_data.pkl", 
+                      "-epochs", "10", "-batchsize", "512", "-dim", "64"])
+    line_subprocess.wait()
+    
+    # Running kmeans on the embeddings and extracting the NMI score
+    result_line = subprocess.run(["python", "LINEImplementation/MyImplementation/kmeans.py"], 
+                                 capture_output=True, text=True, check=True)
+    output_line = result_line.stdout.strip()
+    nmi_line = output_line.split("NMI Score:")[-1].strip() # obtaining last chunk of text (hopefully the float representing the NMI)
+
+    writer.writerow(["LINE", nmi_line])
+
+
+    # Learning the node embeddings using GraRep method
+    print("Running GraRep...")
+    grarep_subprocess = subprocess.Popen(["python", "GraRepImplementation/MyImplementation/main.py", "-g", "./sbm_graph.edgelist", 
+                      "-order", "6", "-dim", "21", "-iters", "20"])
+    grarep_subprocess.wait()
+    
+    # Running kmeans on the embeddings and extracting the NMI score
+    result_grarep = subprocess.run(["python", "GraRepImplementation/MyImplementation/kmeans.py"], 
+                                   capture_output=True, text=True, check=True)
+    output_grarep = result_grarep.stdout.strip()
+    nmi_grarep = output_grarep.split("NMI Score:")[-1].strip() # obtaining last chunk of text (hopefully the float representing the NMI)
+
+    writer.writerow(["GraRep", nmi_grarep])
+
+print(f"The NMI scores for each of the methods have been written to {FILENAME}")
---
+++ sbm_simulator.py
@@ -0,0 +1,116 @@
+import networkx as nx
+import numpy as np
+import random
+from scipy.stats import bernoulli
+import matplotlib.pyplot as plt
+import pickle
+
+## Simulating an SBM
+
+# Create empty graph
+G = nx.Graph()
+
+NUM_NODES = 1000
+
+# Possible positions/roles for someone at the university
+positions = ["undergraduate", "graduate", "faculty"]
+
+# A convenient mapping from positions to integers
+enumerate_positions = {"undergraduate":0, "graduate":1, "faculty":2}
+
+# Probability of a person at the university being having each position 
+position_probs = [1/3, 1/3, 1/3]
+
+# Sampling a position for each person
+actual_positions = random.choices(positions, position_probs, k=NUM_NODES)
+
+# Sorting the positions (with undergraduate < graduate < faculty) to enable comparison 
+# with predicted positions/clusters later
+actual_positions.sort(key = lambda position: enumerate_positions[position])
+
+# Adding a node for each person, along with their position
+for node in range(NUM_NODES):
+    G.add_node(node+1, position = actual_positions[node])
+
+# The matrix defining the probability of a person in position i
+# and a person in position j being friends (having an edge between them)
+connectivity_matrix = np.array([[0.5, 0.1, 0.1],
+                                [0.1, 0.7, 0.1],
+                                [0.1, 0.1, 0.9]
+                      ])
+
+# Obtaining the integer representation of each position (to index into connectivity matrix)
+node_blocks = np.zeros(shape=NUM_NODES, dtype=int)
+for i, position in enumerate(actual_positions):
+    node_blocks[i] = enumerate_positions[actual_positions[i]]
+
+# Using the connectivity matrix to produce the graph's edges 
+for i in range(NUM_NODES):
+    for j in range(i):
+        # Getting the probability of an edge connection between node i and j
+        connection_prob = connectivity_matrix[node_blocks[i], node_blocks[j]]
+        has_edge = bernoulli.rvs(connection_prob) # Sampling a bernoulli distribution with the predefined connection probability
+        if has_edge:
+            # Adding an edge if a success (1) was sampled
+            G.add_edge(i+1, j+1) # adding 1 since node labels in graph start from 1, not 0
+
+clusters = [] # A 2D list which will store nodes in the same cluster in their own row
+# Adding all the nodes in the same cluster to the 2D list, cluster by cluster
+for i in range(len(positions)):
+    this_cluster = node_blocks[node_blocks == i] # filtering for all nodes in clustering i
+    clusters.append(this_cluster)
+
+node_list = [] # A 2D list, with the same shape as the 2D list clusters, where each entry is the node's label/ID
+first_ID = 1 # A counter variable to keep track of node IDs across different clusters
+for cluster in clusters:
+    this_list = list(range(first_ID, first_ID+len(cluster))) # list of node IDs for nodes in "this" cluster
+    node_list.append(this_list)
+
+    first_ID = first_ID+len(cluster) # update firstID to be the ID of the first node in the next cluster
+
+
+## Plotting an SBM
+
+cluster_centers = [(-5, 5), (5, 5), (0, -5)] # Coordinates of cluster centres
+
+pos = {}
+# Iterate over all clusters
+for cluster, nodes in enumerate(node_list):
+    # Subgraph with only nodes in this cluster
+    cluster_G = G.subgraph(nodes)
+
+    # Generate the subpositions for each node ("noise" position added to cluster position)
+    sub_pos = nx.spring_layout(cluster_G, center=(0, 0), scale=1.0, k=3)
+
+    # Offset positions by the cluster center
+    offset = np.array(cluster_centers[cluster])
+    for node, coord in sub_pos.items():
+        pos[node] = coord + offset
+
+# Create a mapping from positions to colours, for plotting
+node_colours = ['tab:blue', 'tab:orange', 'tab:green']
+
+# Plot nodes, one cluster at a time
+for nodeIDs, clr in zip(node_list, node_colours):
+    nx.draw_networkx_nodes(G, pos, nodelist=nodeIDs, node_color=clr, node_size=50)
+
+# Plot edges
+nx.draw_networkx_edges(G, pos)
+
+plt.title("Clustered Layout: Nodes Positioned by Cluster")
+plt.axis("off")
+
+# Saving the plot
+plt.savefig("sbm.png")
+
+# Saving the list of cluster membership (0, 1, or 2) of each node
+with open("sbm_actual_labels.pkl", "wb") as f:
+    pickle.dump({"Graph": G, "Block/Cluster": node_blocks}, f)
+
+# Saving the graph
+nx.write_edgelist(G, "sbm_graph.edgelist", data=False)
+
+# Specifying the edge-weight to be 1 for every edge
+with open("sbm_graph.edgelist", "w") as f:
+    for u, v in G.edges():
+        f.write(f"{u} {v} 1\n")
